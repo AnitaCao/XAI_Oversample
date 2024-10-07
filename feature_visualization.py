c5d99d10773db25e37757cafe8d4a3e857471609
Ting Cao <anita_cao@live.com>
2024-10-07 09:31:31 -0400

    updated feature_visualization to include different type of dataset

--- feature_visualization.py
+++ feature_visualization.py
@@ -11,8 +11,10 @@ import seaborn as sns
 from CMO.imbalance_data.imbalance_iNaturalist import load_imb_inaturalist, Imb_iNat_Dataset
 
 # Load the pretrained ResNet-50 model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = models.resnet50(pretrained=True)
 model.eval()
+model.to(device)
 
 def get_features(module, input, output):
     features.append(output.view(output.size(0), -1).detach().cpu().numpy())
@@ -29,7 +31,9 @@ transform = transforms.Compose([
 ])
 
 data_path = "D:/anita/Research/iNaturalist/train/"
-train_images_list, train_labels_list, _, _ = load_imb_inaturalist(data_path, transform, transform)
+long_tail = False
+random_select = False
+train_images_list, train_labels_list, _, _ = load_imb_inaturalist(data_path, transform, transform, long_tail, random_select)
 train_dataset = Imb_iNat_Dataset(data_path,train_images_list, train_labels_list,transform)
 
 dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False)
@@ -38,6 +42,7 @@ dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False)
 labels = []
 with torch.no_grad():
     for inputs, targets in dataloader:
+        inputs = inputs.to(device)
         _ = model(inputs)
         labels.extend(targets.numpy())
 
@@ -57,20 +62,12 @@ plt.title('Pairwise Distance Matrix of Features')
 plt.show()
 '''
 
+
 # Reduce dimensionality using t-SNE
-tsne = TSNE(n_components=2, random_state=42)
+tsne = TSNE(n_components=2, random_state=42, perplexity=10)
 reduced_features = tsne.fit_transform(features)
 
 class_labels = np.unique(labels)
-
-# Plot the reduced features
-plt.figure(figsize=(10, 8))
-scatter = plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=labels, cmap='viridis', alpha=0.7)
-plt.legend(handles=scatter.legend_elements(), labels=class_labels)
-plt.colorbar(scatter)
-plt.title('t-SNE of ResNet-50 Features')
-plt.show()
-
 
 # Plot the reduced features
 plt.figure(figsize=(10, 8))
@@ -85,6 +82,6 @@ for label in class_labels:
     class_points = reduced_features[labels == label]
     centroid = class_points.mean(axis=0)
     class_centroids.append(centroid)
-    plt.annotate(train_dataset.classes[label], (centroid[0], centroid[1]), fontsize=12, ha='center', backgroundcolor='white')
+    plt.annotate(label, (centroid[0], centroid[1]), fontsize=10, ha='center', backgroundcolor='white')
 
 plt.show()
